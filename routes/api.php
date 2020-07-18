58940bc108d092e3f2fe15d87209501e36df0495
Aakash Pawar <aakashpawar1999@gmail.com>
2020-07-18 15:34:59 +0530

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//  Purchase API Routes
+Route::get('transactions', 'PurchaseController@transactionDetailsShowAll');
+Route::get('transaction/{id}/details', 'PurchaseController@transactionDetailsShow');
